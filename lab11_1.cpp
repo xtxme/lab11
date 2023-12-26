f2dbb0850d40f6c1ae5727278f0b48ab55fea1ac
Tanapron Tangpadungsuk <tt.icy013@gmail.com>
2023-12-26 10:37:06 +0700

    lab11.1 Complete

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,30 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int main()
+{
+    
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+
+    srand(time(0));
+    int x = rand()%9;
+    if(x == 0) cout << "You will get A in this 261102.";
+    else if (x == 1) cout << "You will get B+ in this 261102.";
+    else if (x == 2) cout << "You will get B in this 261102.";
+    else if (x == 3) cout << "You will get C+ in this 261102.";
+    else if (x == 4) cout << "You will get C in this 261102.";
+    else if (x == 5) cout << "You will get D+ in this 261102.";
+    else if (x == 6) cout << "You will get D in this 261102.";
+    else if (x == 7) cout << "You will get F in this 261102.";
+    else if (x == 8) cout << "You will get W in this 261102.";
+
+    return 0;
+
+
+}
+
---
+++ retry.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    double value;
+    cout << "Enter your value: ";
+    cin >> value;
+    cout << "Output = " << 2+value;
+}
